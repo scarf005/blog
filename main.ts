e2674ba676dbb803e8673d68d3fbe0d47a7baf65
scarf <greenscarf005@gmail.com>
2023-11-11 11:25:32 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+import { Article, Lang, loadPost, Post, PostFile, toPostOfLocale } from "./load_post.ts"
+import { walk } from "https://deno.land/std@0.201.0/fs/walk.ts"
+import { asynciter } from "https://deno.land/x/asynciter@0.0.18/mod.ts"
+
 const toDate = (date: Date) => date.toISOString().split("T")[0]
 
-const style = /*css*/`
+const style = /*css*/ `
     body {
         margin: auto;
         max-width: 100dvh;
@@ -18,7 +22,7 @@ const style = /*css*/`
     }
 `
 
-const head = /*html*/`
+const head = /*html*/ `
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -29,64 +33,67 @@ const head = /*html*/`
         </style>
     </head>
 `
+
 // https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png
+const entries = await asynciter(walk("posts/", { includeDirs: false, exts: [".ts"] }))
+	.concurrentUnorderedMap(({ path }) => loadPost(path))
+	.collect()
 
-export type Post = {
-	title: string
-	date: Date
-	summary: string
-	path: string
-	post?: string
-}
+console.log(entries)
+// const posts: Post[] = [
+// 	{
+// 		date: new Date("2023-09-06"),
+// 		path: "/posts/etc/simple-is-best",
+// 		lang: {
+// 			ko: {
+// 				title: "간단한 게 최고",
+// 				summary: "왜 프레임워크 없이 블로그를 만들려는 걸까?",
+// 				post: "",
+// 			},
+// 		},
+// 	},
+// ]
 
-const posts: Post[] = [
-	{
-		title: "간단한 게 최고",
-		date: new Date("2023-09-06"),
-		summary: "왜 프레임워크 없이 블로그를 만들려는 걸까?",
-		path: "/posts/etc/simple-is-best",
-	},
-]
+// const previews = (posts: Post[]): string =>
+// 	posts
+// 		.flatMap(toPostOfLocale("ko"))
+// 		.map(({ path, date, title, summary }) => /*html*/ `
+//             <li>
+//                 <a href="${path}">
+//                     <h3>${title}</h3>
+//                 </a>
+//                 <span>${toDate(date)}</span>
+//                 <p>
+//                     ${summary}
+//                 </p>
+//             </li>
+//         `)
+// 		.join(/*html*/ `<hr />`)
 
-const previews = (posts: Post[]): string =>
-	posts.map(({ path, title, summary, date }) => /*html*/ `
-        <li>
-            <a href="${path}">
-                <h3>${title}</h3>
-            </a>
-            <span>${toDate(date)}</span>
-            <p>
-                ${summary}
-            </p>
-        </li>
-        <hr />
-    `)
-		.join("\n")
+// const body = /*html*/ `
+//     <body>
+//         <header>
+//             <h1>
+//                 /home/scarf/
+//             </h1>
+//         </header>
+//         <hr />
+//         <main>
+//             <ul>
+//                 ${previews(posts)}
+//             </ul>
+//         </main>
+//     </body>
+// `
 
-const body = /*html*/`
-    <body>
-        <header>
-            <h1>
-                /home/scarf/
-            </h1>
-        </header>
-        <hr />
-        <main>
-            <ul>
-                ${previews(posts)}
-            </ul>
-        </main>
-    </body>
-`
+// const template = /*html*/ `
+//     <!DOCTYPE html>
+//         ${head}
+//         ${body}
+//     </html>
+// `
 
-const template = /*html*/`
-    <!DOCTYPE html>
-        ${head}
-        ${body}
-    </html>
-`
-
-Deno.serve(
-	{ port: 8080 },
-	(_req) => new Response(template, { headers: { "content-type": "text/html" } }),
-)
+// Deno.serve(
+// 	{ port: 8080 },
+// 	(_req) => new Response(template, { headers: { "content-type": "text/html" } }),
+// )
