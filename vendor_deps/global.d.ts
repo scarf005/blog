8c30921a30ba255cf25d1c8b16e0c15629a0ac70
scarf <greenscarf005@gmail.com>
2023-11-11 18:58:19 +0900

    wip: remove preact

--- vendor_deps/global.d.ts
+++
@@ -1,8 +0,0 @@
-// deno-lint-ignore-file no-empty-interface
-import { JSXInternal } from "./jsx.d.ts"
-
-declare global {
-	namespace JSX {
-		interface IntrinsicElements extends JSXInternal.IntrinsicElements {}
-	}
-}
--- vendor_deps/preact-render-to-string.d.ts
+++
@@ -1,8 +0,0 @@
-// deno-lint-ignore-file
-import { VNode } from "./preact.d.ts"
-
-export default function renderToString(vnode: VNode, context?: any): string
-
-export function render(vnode: VNode, context?: any): string
-export function renderToString(vnode: VNode, context?: any): string
-export function renderToStaticMarkup(vnode: VNode, context?: any): string
