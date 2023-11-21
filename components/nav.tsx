f6da9f85ed14feaaa509a94f3774e749bfa11a91
scarf <greenscarf005@gmail.com>
2023-11-21 12:37:04 +0900

    fix: no link for current page

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,6 +3,6 @@ export const Nav = ({ href }: Props) => (
 	<nav>
 		<a title="page home" href="/">/home</a>
 		<a title="github address" href="/scarf005.html">/scarf</a>
-		{href && <a title="current page" href={href}>{href}</a>}
+		{href}
 	</nav>
 )
