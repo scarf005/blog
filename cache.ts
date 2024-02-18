3db2e0252298d91deacc48bb4f1b989f4ae09c6e
scarf <greenscarf005@gmail.com>
2024-02-18 13:51:11 +0900

    chore: remove unused posts

--- cache.ts
+++
@@ -1,3 +0,0 @@
-import map from "./import_map.json" with { type: "json" }
-
-console.log(`${map.imports["lume/"]}cli.ts`)
--- posts/etc/websockets.tsx
+++
@@ -1,7 +0,0 @@
-export const title = "웹소켓"
-
-export default () => (
-	<>
-		<p>어려워!</p>
-	</>
-)
