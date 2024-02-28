0e5770d3adcf9d41e57390a5ef6e8469960801f8
scarf <greenscarf005@gmail.com>
2024-02-28 11:32:17 +0900

    perf: prefetch pages (#8)
    network cost doesn't matter much as all pages combined costs less than 1MiB

--- assets/flamethrower/flamethrower.js
+++ assets/flamethrower/flamethrower.js
@@ -1,2 +1,3 @@
 import flamethrower from "https://esm.sh/v134/flamethrower-router@0.0.0-meme.12"
-flamethrower({ log: false, pageTransitions: true })
+
+flamethrower({ log: false, prefetch: "visible", pageTransitions: true })
