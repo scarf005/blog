110c89664d266308e0547df97b71bbc239efc49c
scarf <greenscarf005@gmail.com>
2024-03-05 15:35:33 +0900

    perf: pre-resolve import URL resolution
    removes one roundtrip

--- assets/flamethrower/flamethrower.js
+++ assets/flamethrower/flamethrower.js
@@ -1,4 +1,5 @@
-import flamethrower from "https://esm.sh/v134/flamethrower-router@0.0.0-meme.12"
+// @deno-types="https://esm.sh/v135/flamethrower-router@0.0.0-meme.12"
+import flamethrower from "https://esm.sh/v135/flamethrower-router@0.0.0-meme.12/es2022/flamethrower-router.mjs"
 
 flamethrower({
 	log: globalThis.location.hostname === "localhost",
