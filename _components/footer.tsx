d037df7bb7eb261c41794d2bb1b9d093583e5497
scarf <greenscarf005@gmail.com>
2024-09-04 15:19:18 +0900

    feat: update license date

--- _components/footer.tsx
+++ _components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => (
 	<footer>
-		© 2023 <a href="https://github.com/scarf005">scarf</a> |{" "}
+		© 2023-{date.getFullYear()} <a href="https://github.com/scarf005">scarf</a> |{" "}
 		<a href="https://www.gnu.org/licenses/agpl-3.0.en.html">AGPL-3.0-Only</a> |{" "}
 		<a href="https://www.github.com/scarf005/blog">Source</a>
 	</footer>
