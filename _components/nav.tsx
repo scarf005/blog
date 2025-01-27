d618b38cfd11219986c09f4de3d53d94fcb0e6d2
scarf <greenscarf005@gmail.com>
2025-01-28 03:53:43 +0900

    feat: relative link icons

--- _components/nav.tsx
+++ _components/nav.tsx
@@ -1,14 +1,14 @@
 type Props = { href?: string }
 export const Nav = ({ href }: Props) => {
 	const sections = href?.replace("/", "").split("/").map((x) => `/${x}`)
-	const links = sections?.slice(0, -1).map((x) => <a title={x} href={x + "/index.html"}>{x}</a>)
+	const links = sections?.slice(0, -1).map((x) => <a title={x} href={x + "/index.html"} data-no-icon>{x}</a>)
 
 	const last = sections?.at(-1)
 
 	return (
 		<nav>
-			<a title="홈" href="/index.html">/home</a>
-			<a title="소개" href="/scarf005.html">/scarf</a>
+			<a title="홈" href="/index.html" data-no-icon>/home</a>
+			<a title="소개" href="/scarf005.html" data-no-icon>/scarf</a>
 			{links}
 			{last}
 		</nav>
