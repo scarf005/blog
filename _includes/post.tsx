d89fe51151dc5520f9fa22beee894c72c685fe48
scarf <greenscarf005@gmail.com>
2025-08-10 23:45:24 +0900

    feat: show modified date

--- _includes/post.tsx
+++ _includes/post.tsx
@@ -1,12 +1,18 @@
+import { getGitDate } from "lume/core/utils/date.ts"
 import { DateRange } from "~/_components/date.tsx"
 
 export const layout = "base.tsx"
-export default ({ title, url, date, children }: Lume.Data, _helpers: Lume.Helpers) => (
-	<>
-		<header>
-			<h1>{title ?? url}</h1>
-			<DateRange date={date} modifiedDate={date} />
-		</header>
-		<main>{children}</main>
-	</>
-)
+export default ({ title, url, date, children, page }: Lume.Data, _helpers: Lume.Helpers) => {
+	const src = page.src.entry?.src
+	const modifiedDate = src ? getGitDate("modified", src) : undefined
+
+	return (
+		<>
+			<header>
+				<h1>{title ?? url}</h1>
+				<DateRange date={date} modifiedDate={modifiedDate ?? date} />
+			</header>
+			<main>{children}</main>
+		</>
+	)
+}
